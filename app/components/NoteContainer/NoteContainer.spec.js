ac54690a5cd5ce17080a5568faebfcb75e137f93
Otto Kivikarki <ottokivikarki@Otto-MacBook-Air.local>
2016-03-06 17:28:08 +0200

    Added Karma for testing and initial tests

---
+++ app/components/NoteContainer/NoteContainer.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+
+import { NoteContainer } from './NoteContainer.jsx';
+import { NoteTextArea } from './../NoteTextArea/NoteTextArea.jsx';
+
+expect.extend(expectJSX);
+
+var component, renderer;
+
+describe('NoteContainer', () => {
+
+    beforeEach(() => {
+        component = TestUtils.renderIntoDocument(<NoteContainer/>);
+        renderer = TestUtils.createRenderer();
+        renderer.render(<NoteContainer/>);
+    });
+
+    it('should contain a textarea', () => {
+        let textArea = TestUtils.findRenderedDOMComponentWithTag(component, 'textarea');
+        expect(textArea).toExist();
+    });
+
+    it('should render a div with css class', () => {
+        let result = renderer.getRenderOutput();
+        expect(result.type).toEqual('div');
+        expect(result.props.className).toEqual('notecontainer');
+    });
+});
